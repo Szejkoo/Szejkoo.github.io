9c9370253988260ad949de95e524727e10b54023
Szejkoo <61558465+Szejkoo@users.noreply.github.com>
2020-12-11 14:24:02 +0100

    Add files via upload

---
+++ kox/skrypt.js
@@ -0,0 +1,12 @@
+function openNav() {
+  document.getElementById("myNav").style.height = "100%";
+}
+
+function closeNav() {
+  document.getElementById("myNav").style.height = "0%";
+}
+
+
+$(window).on("load",function(){
+  $(".loader1").fadeOut(4000);
+});
